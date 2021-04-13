a58ecb3f4e443126c75cff7a126e8e03b7ba9d31
BEAST GLATISANT <26509147+g147@users.noreply.github.com>
2021-04-13 13:14:57 +0530

    kenzerdb[2.8]

--- scripts/remove_logs.sh
+++ scripts/remove_logs.sh
@@ -1,4 +1,4 @@
 #rm ../directory/*/*.old*
 rm ../directory/*/*.log*
-rm -r ../directory/*/nuclei ../directory/*/jaeles ../directory/*/nxscan ../directory/*/gocrawler
+rm -r ../directory/*/nuclei ../directory/*/jaeles ../directory/*/passive-jaeles ../directory/*/nxscan ../directory/*/gocrawler
 find ../directory -type f -empty -delete
